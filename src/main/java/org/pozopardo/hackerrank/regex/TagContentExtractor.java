99e5f0465d101bba85dbe07339143a6a388744fe
Denis Pozo <denis.pozo@dai-labor.de>
2019-02-08 19:20:23 +0100

    add tag content extractor exercies and abstract class

--- src/main/java/org/pozopardo/hackerrank/regex/TagContentExtractor.java
+++ src/main/java/org/pozopardo/hackerrank/regex/TagContentExtractor.java
@@ -1,43 +1,55 @@
 package org.pozopardo.hackerrank.regex;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
 import java.util.regex.*;
 
 public class TagContentExtractor {
 
-    private static final Pattern PATTERN = Pattern.compile("<(.*)>(.+)</\\1>");
+    private static final Pattern PATTERN = Pattern.compile("<(.+)>([^<>]+)</\\1>");
+    private static List<String> content;
 
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
+    public static String extractContent(String line) {
+        content = new ArrayList<>();
+        findMatches(line);
+        return printInnerTagContent(content);
+    }
 
-        while(testCases>0){
-            String line = in.nextLine();
-            printInnerTagContent(line);
-            testCases--;
+    private static void findMatches(String line) {
+        Matcher matcherIn = PATTERN.matcher(line);
+
+        while(matcherIn.find()) {
+            content.add(matcherIn.group(2));
         }
     }
 
-    private static void printInnerTagContent(String line) {
-        String result = null;
-        Matcher matcherIn = PATTERN.matcher(line);
+    private static String printInnerTagContent(List<String> content) {
 
-        while (matcherIn.find()){
-            Matcher matcherRecursive = PATTERN.matcher(matcherIn.group(2));
-            if(!matcherRecursive.matches()){
-                result = matcherIn.group(2);
-            } else {
-                printInnerTagContent(matcherIn.group(2));
-                result = "None";
-            }
+        if(content.size() == 0) {
+            return "None\n";
         }
 
-        if(result != null) {
-            System.out.println(result);
+        StringBuilder builder = new StringBuilder();
+//        for(int i = 0 ; i < content.size()-1 ; i++) {
+        for(String part : content) {
+            builder.append(part);
+            builder.append("\n");
         }
+//        builder.append(content.get(content.size()-1));
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        while (testCases > 0) {
+            String line = in.nextLine();
+            System.out.print(extractContent(line));
+            testCases--;
+        }
+
+        in.close();
     }
 }
 
--- src/test/java/org/pozopardo/hackerrank/regex/TagContentExtractorTest.java
+++ src/test/java/org/pozopardo/hackerrank/regex/TagContentExtractorTest.java
@@ -1,7 +1,43 @@
 package org.pozopardo.hackerrank.regex;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junitparams.JUnitParamsRunner.$;
+
+@RunWith(JUnitParamsRunner.class)
 public class TagContentExtractorTest {
 
 
+    private static final Object[] getTestStrings() {
+       return $(
+               $("<h1>Nayeem loves counseling</h1>", "Nayeem loves counseling\n"),
+               $("<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>",
+                       "Sanjay has no watch\nSo wait for a while\n"),
+               $("<Amee>safat codes like a ninja</amee>", "None\n"),
+               $("<h1>some</h1>", "some\n"),
+               $("<h1>had<h1>public</h1></h1>", "public\n"),
+               $("<h1>had<h1>public</h1515></h1>", "None\n"),
+               $("<h1><h1></h1></h1>", "None\n"),
+               $("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", "None\n"),
+               $(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", "None\n"),
+               $("<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>", "None\n"),
+               $("<>hello</>", "None\n"),
+               $("<>hello</><h>dim</h>", "dim\n"),
+               $("<>hello</><h>dim</h>>>>>", "dim\n"),
+               $("<SA premium>Imtiaz has a secret crush</SA premium>",
+                       "Imtiaz has a secret crush\n")
+       );
+    }
 
+    @Test
+    @Parameters(method = "getTestStrings")
+    public void testExtractorAgainstGivenInputOutput(String line, String output)
+    {
+        String result = TagContentExtractor.extractContent(line);
+        Assert.assertEquals(output, result);
+    }
 }
