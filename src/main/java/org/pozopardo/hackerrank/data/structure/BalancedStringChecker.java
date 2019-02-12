76d3337183ff5e4b43686a304a73280cd156f97d
Denis Pozo <denis.pozo@dai-labor.de>
2019-02-12 20:17:39 +0100

    add balanced string checker

---
+++ src/main/java/org/pozopardo/hackerrank/data/structure/BalancedStringChecker.java
@@ -0,0 +1,76 @@
+package org.pozopardo.hackerrank.data.structure;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalancedStringChecker {
+
+    public static boolean isBalanced(String string) {
+
+        if (string.length() == 1)
+            return false;
+
+        Stack<Character> charStack = new Stack<>();
+        int index = 0;
+
+        while (index < string.length()) {
+            Character c = string.charAt(index++);
+
+            if (isOpening(c)) {
+                charStack.push(c);
+            } else if (isClosing(c) && !charStack.isEmpty()) {
+                Character fromStack = charStack.pop();
+                if(!opposite(c, fromStack)){
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        if(charStack.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean opposite(Character closing, Character opening) {
+        if(opening == '{' && closing == '}')
+            return true;
+        if(opening == '[' && closing == ']')
+            return true;
+        if(opening == '(' && closing == ')')
+            return true;
+        return false;
+    }
+
+    private static boolean isOpening(Character c) {
+        if(c == '{' || c == '[' || c == '(')
+            return true;
+        return false;
+    }
+
+    private static boolean isClosing(Character c) {
+        if(c == '}' || c == ']' || c == ')')
+            return true;
+        return false;
+    }
+
+    public static void main(String [] args) {
+
+        {
+            Scanner sc = new Scanner(System.in);
+
+            while (sc.hasNext()) {
+                String input=sc.next();
+                if(isBalanced(input))
+                    System.out.println("true");
+                else
+                    System.out.println("false");
+            }
+
+        }
+    }
+}
