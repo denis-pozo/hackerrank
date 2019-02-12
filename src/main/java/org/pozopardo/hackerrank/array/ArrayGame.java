d575dfb07849701174c76d9eb852be1061d832fc
Denis Pozo <denis.pozo@dai-labor.de>
2019-02-12 14:03:50 +0100

    add array game test finished

--- src/main/java/org/pozopardo/hackerrank/array/ArrayGame.java
+++ src/main/java/org/pozopardo/hackerrank/array/ArrayGame.java
@@ -5,41 +5,86 @@ import java.util.Scanner;
 public class ArrayGame {
 
     public static boolean canWin(int leap, int[] game) {
-       int currentIndex = 0;
-       boolean gameOver = false;
+        boolean gameOver = false;
+        boolean win = false;
+        int returnedIndex = 0;
+        int count = 0;
 
-       while(currentIndex < game.length && !gameOver) {
-           if(canJump(currentIndex + leap, game)){
-               currentIndex = currentIndex + leap;
-           } else if (canMoveForward(currentIndex, game)){
-               currentIndex++;
-           } else {
-               gameOver = true;
-           }
-       }
-        return !gameOver;
+        while(!gameOver && !win) {
+            returnedIndex = play(leap, game);
+            count++;
+            if(returnedIndex < game.length) {
+                if(returnedIndex == 0){
+                    gameOver = true;
+                } else {
+                    game[returnedIndex] = 1;
+                }
+            } else {
+                win = true;
+            }
+        }
+
+        System.out.println("# of Plays: " + count);
+        return win;
     }
 
-    private static boolean canJump(int newIndex, int[] game) {
-        if(newIndex >= game.length ) return true;
-        if(game[newIndex] == 0) return true;
-        return false;
+    public static int play(int leap, int[] game) {
+        int currentIndex;
+        int nextIndex = 0;
+
+        while(nextIndex < game.length) {
+            currentIndex = indexBeforeNextOne(game, nextIndex);
+            nextIndex = nextMove(game, leap, currentIndex);
+
+            if(currentIndex >= nextIndex)
+                return nextIndex;
+        }
+
+        return nextIndex;
     }
 
-    static boolean canMoveForward(int currentIndex, int[] game){
-        if(currentIndex == game.length-1) return true;
-        if(game[currentIndex+1] == 0) return true;
-        return false;
+    private static int nextMove(int[] game, int leap, int currentIndex) {
+        if(currentIndex == game.length-1){
+            return game.length;
+        }
+
+        int jumps = leap;
+        while(jumps-- > 1) {
+            if(canJump(game, currentIndex + leap)){
+                currentIndex = currentIndex + leap;
+                return currentIndex;
+            } else if (canWalkBackward(game, currentIndex)){
+                currentIndex--;
+            } else {
+                return currentIndex;
+            }
+        }
+
+        return currentIndex;
     }
 
-    static boolean canMoveBackward(int currentIndex, int[] game){
+    private static boolean canWalkBackward(int[] game, int currentIndex) {
         if(currentIndex == 0) return false;
-        if(game[currentIndex-1] == 0) return true;
-        return false;
+        if(game[currentIndex-1] == 1) return false; else return true;
+    }
+
+    private static boolean canJump(int[] game, int jump) {
+        if(jump >= game.length) return true;
+        if(game[jump] == 0) return true; else return false;
+    }
+
+    private static int indexBeforeNextOne(int[] game, int currentIndex) {
+
+        for(int i = currentIndex ; i < game.length ; i++) {
+            if(game[i] == 1) return i-1;
+        }
+
+        return game.length-1;
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int q = scan.nextInt();
         while (q-- > 0) {
             int n = scan.nextInt();
@@ -49,8 +94,7 @@ public class ArrayGame {
             for (int i = 0; i < n; i++) {
                 game[i] = scan.nextInt();
             }
-
-            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+            System.out.println((canWin(leap, game)) ? "YES" : "NO" );
         }
         scan.close();
     }
