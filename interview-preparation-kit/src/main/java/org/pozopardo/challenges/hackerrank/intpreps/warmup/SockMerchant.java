799932af3c0f520653583e9056584699d123e4c3
Denis Pozo <denis.pozo@dai-labor.de>
2019-05-08 00:07:25 +0200

    add solution of the sockmerchant problem

--- interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/warmup/SockMerchant.java
+++ interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/warmup/SockMerchant.java
@@ -1,4 +1,50 @@
 package org.pozopardo.challenges.hackerrank.intpreps.warmup;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class SockMerchant {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        Set<Integer> unpaired = new HashSet<>();
+
+        for(Integer color : ar) {
+            if (unpaired.contains(color)) unpaired.remove(color);
+            else unpaired.add(color);
+        }
+
+        return (n - unpaired.size()) / 2;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        System.out.println(String.valueOf(result));
+
+        scanner.close();
+    }
 }
+
