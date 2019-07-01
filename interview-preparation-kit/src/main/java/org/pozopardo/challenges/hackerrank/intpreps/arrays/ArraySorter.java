80916eb3187f920aef3b9f82f714284608e9a649
Denis Pozo <denispzpd@gmail.de>
2019-07-01 15:24:19 +0200

    create file for next challenge - arraysorter

---
+++ interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/arrays/ArraySorter.java
@@ -0,0 +1,41 @@
+package org.pozopardo.challenges.hackerrank.intpreps.arrays;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ArraySorter {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+        System.out.println(res);
+
+        scanner.close();
+    }
+}
