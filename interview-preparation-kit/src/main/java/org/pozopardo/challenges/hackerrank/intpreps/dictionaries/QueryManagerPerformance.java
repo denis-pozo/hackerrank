7c47bee71e06e595cbe0c40da2e1a46f488cade8
Denis Pozo <denispzpd@gmail.de>
2019-07-23 21:31:38 +0200

    current status, still not enough performing

--- interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/dictionaries/QueryManagerPerformance.java
+++ interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/dictionaries/QueryManagerPerformance.java
@@ -17,80 +17,61 @@ public class QueryManagerPerformance {
     private static final int CHECK = 3;
 
     // Complete the freqQuery function below.
-
-    static List<Integer> freqQuery(List<List<Integer>> queries) {
-//        static List<Integer> freqQuery(int [][] queries) {
+    static List<Integer> freqQuery(int [][] queries) {
         List<Integer> result = new ArrayList<>();
-        List<Integer> data = new ArrayList<>();
-        HashMap<Integer, Function<Integer, Boolean>> mapa = new HashMap<>();
-        mapa.put(1, o -> data.add(o));
-        mapa.put(2, o -> data.remove(o));
-        mapa.put(3, o -> {
-            Map<Integer, Long> counts = data.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
-            return result.add(counts.containsValue(new Long(o))? 1:0);
-        });
-        queries.forEach(integers -> {
-            mapa.get(integers.get(0)).apply(integers.get(1));
-        });
-        return  result;
+        Map<Integer, Integer> numbers = new HashMap<>();
+        Map<Integer, Integer> frequencies = new HashMap<>();
+        int operation = 0;
+        int value = -1;
+        int prevRes = -1;
+        Integer oldFreq;
+        Integer newFreq;
+        Integer oldOccurrence;
+        Integer newOccurrence;
 
-//        List<Integer> result = new ArrayList<>();
-//        Map<Integer, Integer> numbers = new HashMap<>();
-//        Map<Integer, Integer> frequencies = new HashMap<>();
-//        int operation = 0;
-//        int value = -1;
-//        int prevRes = -1;
-//        Integer oldFreq;
-//        Integer newFreq;
-//        Integer oldOccurrence;
-//        Integer newOccurrence;
-//
-//        for(int [] query : queries) {
-//            if(query[1] < 1 || query[1] > 1000000000) {
-//                System.out.println("Hello");
-//            } else {
-//                if(operation == 3 && query[0] == 3 && value == query[1]) {
-//                    result.add(prevRes);
-//                } else {
-//                    operation = query[0];
-//                    value = query[1];
-//
-//                    if (operation == 3) {
-//                        prevRes = frequencies.get(value) == null ? 0 : 1;
-//                        result.add(prevRes);
-//
-//                    } else if ((operation == 2 && numbers.containsKey(value)) || operation == 1) {
-//                        oldFreq = numbers.get(value);
-//                        oldFreq = oldFreq == null ? 0 : oldFreq;
-//                        oldOccurrence = frequencies.get(oldFreq);
-//                        oldOccurrence = oldOccurrence == null ? 0 : oldOccurrence;
-//
-//                        if (operation == 1) {
-//                            newFreq = oldFreq + 1;
-//                        } else {
-//                            newFreq = oldFreq - 1;
-//                        }
-//
-//                        newOccurrence = frequencies.get(newFreq);
-//                        newOccurrence = newOccurrence == null ? 0 : newOccurrence;
-//
-//                        if (newFreq < 1) {
-//                            numbers.remove(value);
-//                        } else {
-//                            numbers.put(value, newFreq);
-//                        }
-//
-//                        if ((oldOccurrence - 1) < 1) {
-//                            frequencies.remove(oldFreq);
-//                        } else {
-//                            frequencies.put(oldFreq, oldOccurrence - 1);
-//                        }
-//                        frequencies.put(newFreq, newOccurrence + 1);
-//                    }
-//                }
-//            }
-//        }
-//        return result;
+        for(int [] query : queries) {
+            if(operation == 3 && query[0] == 3 && value == query[1]) {
+                result.add(prevRes);
+            } else {
+                operation = query[0];
+                value = query[1];
+
+                if (operation == 3) {
+                    prevRes = frequencies.get(value) == null ? 0 : 1;
+                    result.add(prevRes);
+
+                } else if ((operation == 2 && numbers.containsKey(value)) || operation == 1) {
+                    oldFreq = numbers.get(value);
+                    oldFreq = oldFreq == null ? 0 : oldFreq;
+                    oldOccurrence = frequencies.get(oldFreq);
+                    oldOccurrence = oldOccurrence == null ? 0 : oldOccurrence;
+
+                    if (operation == 1) {
+                        newFreq = oldFreq + 1;
+                    } else {
+                        newFreq = oldFreq - 1;
+                    }
+
+                    newOccurrence = frequencies.get(newFreq);
+                    newOccurrence = newOccurrence == null ? 0 : newOccurrence;
+
+                    if (newFreq < 1) {
+                        numbers.remove(value);
+                    } else {
+                        numbers.put(value, newFreq);
+                    }
+
+                    if ((oldOccurrence - 1) < 1) {
+                        frequencies.remove(oldFreq);
+                    } else {
+                        frequencies.put(oldFreq, oldOccurrence - 1);
+                    }
+                    frequencies.put(newFreq, newOccurrence + 1);
+                }
+            }
+        }
+
+        return result;
 
 //        Map<Integer, Integer> operationsMap = new HashMap();
 //        Map<Integer, Set<Integer>> frequenciesMap = new HashMap<>();
@@ -154,7 +135,7 @@ public class QueryManagerPerformance {
     }
 
     public static void main(String[] args) throws IOException {
-        String fileName = "/query-manager-case13.txt";
+        String fileName = "/query-manager-case9.txt";
         InputStream fileStream = QueryManagerPerformance.class.getResourceAsStream(fileName);
         long startTimeTotal = System.currentTimeMillis();
 
