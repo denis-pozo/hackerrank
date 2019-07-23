268ab905a812ca3be9e66c26e3dd92d4b0738140
Denis Pozo <denispzpd@gmail.de>
2019-07-23 21:18:05 +0200

    trying new stuff

--- interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/dictionaries/QueryManagerPerformance.java
+++ interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/dictionaries/QueryManagerPerformance.java
@@ -1,10 +1,9 @@
 package org.pozopardo.challenges.hackerrank.intpreps.dictionaries;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -19,68 +18,145 @@ public class QueryManagerPerformance {
 
     // Complete the freqQuery function below.
 
-    static List<Integer> freqQuery(int [][] queries) {
+    static List<Integer> freqQuery(List<List<Integer>> queries) {
+//        static List<Integer> freqQuery(int [][] queries) {
         List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> numbers = new HashMap<>();
-        Map<Integer, Integer> frequencies = new HashMap<>();
+        List<Integer> data = new ArrayList<>();
+        HashMap<Integer, Function<Integer, Boolean>> mapa = new HashMap<>();
+        mapa.put(1, o -> data.add(o));
+        mapa.put(2, o -> data.remove(o));
+        mapa.put(3, o -> {
+            Map<Integer, Long> counts = data.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+            return result.add(counts.containsValue(new Long(o))? 1:0);
+        });
+        queries.forEach(integers -> {
+            mapa.get(integers.get(0)).apply(integers.get(1));
+        });
+        return  result;
 
-        int operation = 0;
-        int value = -1;
-        int prevRes = -1;
-        Integer oldFreq;
-        Integer newFreq;
-        Integer oldOccurrence;
-        Integer newOccurrence;
+//        List<Integer> result = new ArrayList<>();
+//        Map<Integer, Integer> numbers = new HashMap<>();
+//        Map<Integer, Integer> frequencies = new HashMap<>();
+//        int operation = 0;
+//        int value = -1;
+//        int prevRes = -1;
+//        Integer oldFreq;
+//        Integer newFreq;
+//        Integer oldOccurrence;
+//        Integer newOccurrence;
+//
+//        for(int [] query : queries) {
+//            if(query[1] < 1 || query[1] > 1000000000) {
+//                System.out.println("Hello");
+//            } else {
+//                if(operation == 3 && query[0] == 3 && value == query[1]) {
+//                    result.add(prevRes);
+//                } else {
+//                    operation = query[0];
+//                    value = query[1];
+//
+//                    if (operation == 3) {
+//                        prevRes = frequencies.get(value) == null ? 0 : 1;
+//                        result.add(prevRes);
+//
+//                    } else if ((operation == 2 && numbers.containsKey(value)) || operation == 1) {
+//                        oldFreq = numbers.get(value);
+//                        oldFreq = oldFreq == null ? 0 : oldFreq;
+//                        oldOccurrence = frequencies.get(oldFreq);
+//                        oldOccurrence = oldOccurrence == null ? 0 : oldOccurrence;
+//
+//                        if (operation == 1) {
+//                            newFreq = oldFreq + 1;
+//                        } else {
+//                            newFreq = oldFreq - 1;
+//                        }
+//
+//                        newOccurrence = frequencies.get(newFreq);
+//                        newOccurrence = newOccurrence == null ? 0 : newOccurrence;
+//
+//                        if (newFreq < 1) {
+//                            numbers.remove(value);
+//                        } else {
+//                            numbers.put(value, newFreq);
+//                        }
+//
+//                        if ((oldOccurrence - 1) < 1) {
+//                            frequencies.remove(oldFreq);
+//                        } else {
+//                            frequencies.put(oldFreq, oldOccurrence - 1);
+//                        }
+//                        frequencies.put(newFreq, newOccurrence + 1);
+//                    }
+//                }
+//            }
+//        }
+//        return result;
 
-        for(int [] query : queries) {
-            if(operation == 3 && query[0] == 3 && value == query[1]) {
-                result.add(prevRes);
-            } else {
-                operation = query[0];
-                value = query[1];
+//        Map<Integer, Integer> operationsMap = new HashMap();
+//        Map<Integer, Set<Integer>> frequenciesMap = new HashMap<>();
+//
+//        for(int[] query : queries) {
+//            int operation = query[0];
+//            int value = query[1];
+//
+//            switch (operation) {
+//                case INSERT:
+//                    Integer m = operationsMap.get(value);
+//                    if( m == null) {
+//                        operationsMap.put(value, 1);
+//                        Set<Integer> sampleMap = frequenciesMap.get(1);
+//                        if(sampleMap != null) {
+//                            sampleMap.add(value);
+//                        } else {
+//                            frequenciesMap.put(1, new HashSet(){
+//                                { add(value); }
+//                            });
+//                        }
+//                    } else {
+//                        operationsMap.put(value, ++m);
+//                        frequenciesMap.get(m - 1).remove(value);
+//                        Set<Integer> sampleMap = frequenciesMap.get(m);
+//                        if(sampleMap != null) sampleMap.add(value);
+//                        else {
+//                            frequenciesMap.put(m, new HashSet() {
+//                                { add(value); }
+//                            });
+//                        }
+//                    }
+//                    break;
+//
+//                case DELETE:
+//                    Integer n = operationsMap.get(value);
+//                    if(n == null) break;
+//                    else if(n == 1) {
+//                        operationsMap.remove(value);
+//                        frequenciesMap.get(1).remove(value);
+//                    } else {
+//                        operationsMap.put(value, --n);
+//                        frequenciesMap.get(n + 1).remove(value);
+//                        Set<Integer> sampleMap = frequenciesMap.get(n);
+//                        if(sampleMap != null)
+//                            sampleMap.add(value);
+//                        else {
+//                            frequenciesMap.put(n, new HashSet() {
+//                                { add(value); }
+//                            });
+//                        }
+//                    }
+//                    break;
+//
+//                case CHECK:
+//                    result.add((frequenciesMap.get(value) == null || frequenciesMap.get(value).isEmpty()) ? 0 : 1);
+//                    break;
+//            }
+//        }
 
-                if (operation == 3) {
-                    prevRes = frequencies.get(value) == null ? 0 : 1;
-                    result.add(prevRes);
-
-                } else if ((operation == 2 && numbers.containsKey(value)) || operation == 1) {
-                    oldFreq = numbers.get(value);
-                    oldFreq = oldFreq == null ? 0 : oldFreq;
-                    oldOccurrence = frequencies.get(oldFreq);
-                    oldOccurrence = oldOccurrence == null ? 0 : oldOccurrence;
-
-                    if (operation == 1) {
-                        newFreq = oldFreq + 1;
-                    } else {
-                        newFreq = oldFreq - 1;
-                    }
-
-                    newOccurrence = frequencies.get(newFreq);
-                    newOccurrence = newOccurrence == null ? 0 : newOccurrence;
-
-                    if (newFreq < 1) {
-                        numbers.remove(value);
-                    } else {
-                        numbers.put(value, newFreq);
-                    }
-
-                    if ((oldOccurrence - 1) < 1) {
-                        frequencies.remove(oldFreq);
-                    } else {
-                        frequencies.put(oldFreq, oldOccurrence - 1);
-                    }
-                    frequencies.put(newFreq, newOccurrence + 1);
-                }
-            }
-        }
-        return result;
     }
 
     public static void main(String[] args) throws IOException {
         String fileName = "/query-manager-case13.txt";
         InputStream fileStream = QueryManagerPerformance.class.getResourceAsStream(fileName);
         long startTimeTotal = System.currentTimeMillis();
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileStream))){
             int q = Integer.parseInt(bufferedReader.readLine().trim());
@@ -95,14 +171,13 @@ public class QueryManagerPerformance {
             List<Integer> ans = freqQuery(queries);
             long endTimeMethod = System.currentTimeMillis();
 
-            bufferedWriter.write(
-                    ans.stream()
-                            .map(Object::toString)
-                            .collect(joining("\n"))
-                            + "\nDuration Total: " + (System.currentTimeMillis()-startTimeTotal) + "\n"
-                            + "Duration Method: " + (endTimeMethod-startTimeMethod) + "\n");
-
-            bufferedWriter.close();
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out))) {
+                         bufferedWriter.write(ans.stream()
+                                 .map(Object::toString)
+                                 .collect(joining("\n"))
+                        + "\nDuration Total: " + (System.currentTimeMillis()-startTimeTotal)
+                        + "\nDuration Method: " + (endTimeMethod-startTimeMethod) + "\n");
+            }
         }
     }
 }
--- interview-preparation-kit/src/test/java/org/pozopardo/challenges/hackerrank/intpreps/dictionaries/QueryManagerTest.java
+++ interview-preparation-kit/src/test/java/org/pozopardo/challenges/hackerrank/intpreps/dictionaries/QueryManagerTest.java
@@ -1,59 +1,57 @@
-package org.pozopardo.challenges.hackerrank.intpreps.dictionaries;
-
-import org.junit.Test;
-
-import java.io.*;
-import java.util.*;
-
-import static java.util.stream.Collectors.joining;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
-public class QueryManagerTest {
-
-    final static private String USECASE_FILENAME = "/query-manager-case10.txt";
-    final static private String RESULTS_FILENAME = "/query-manager-result10.txt";
-
-    @Test
-    public void useCaseFromSite() {
-        long startTime = System.currentTimeMillis();
-        int [][] queries = loadQueries();
-        List<Integer> results = QueryManagerPerformance.freqQuery(queries);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Duration: " + (endTime - startTime));
-        int [] expected = loadResults();
-        assertEquals(expected.length, results.size());
-        for(int i = 0 ; i < expected.length ; i++) {
-            assertEquals(expected[i],results.get(i).intValue());
-        }
-    }
-
-    public static int [][] loadQueries() {
-        InputStream inputStream = QueryManagerTest.class.getResourceAsStream(USECASE_FILENAME);
-        Scanner scanner = new Scanner(inputStream);
-
-        int q = scanner.nextInt();
-        int[][] queries = new int[q][2];
-
-        for (int i = 0; i < q; i++) {
-            queries[i][0] = scanner.nextInt();
-            queries[i][1] = scanner.nextInt();
-        }
-
-        return queries;
-    }
-
-    public static int [] loadResults() {
-        InputStream inputStream = QueryManagerTest.class.getResourceAsStream(RESULTS_FILENAME);
-        Scanner scanner = new Scanner(inputStream);
-
-        int q = scanner.nextInt();
-        int[] results = new int[q];
-
-        for (int i = 0; i < q; i++) {
-            results[i] = scanner.nextInt();
-        }
-
-        return results;
-    }
-}
+//package org.pozopardo.challenges.hackerrank.intpreps.dictionaries;
+//
+//import org.junit.Test;
+//
+//import java.io.*;
+//import java.util.*;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//public class QueryManagerTest {
+//
+//    final static private String USECASE_FILENAME = "/query-manager-case10.txt";
+//    final static private String RESULTS_FILENAME = "/query-manager-result10.txt";
+//
+//    @Test
+//    public void useCaseFromSite() {
+//        long startTime = System.currentTimeMillis();
+//        int [][] queries = loadQueries();
+//        List<Integer> results = QueryManagerPerformance.freqQuery(queries);
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("Duration: " + (endTime - startTime));
+//        int [] expected = loadResults();
+//        assertEquals(expected.length, results.size());
+//        for(int i = 0 ; i < expected.length ; i++) {
+//            assertEquals(expected[i],results.get(i).intValue());
+//        }
+//    }
+//
+//    public static int [][] loadQueries() {
+//        InputStream inputStream = QueryManagerTest.class.getResourceAsStream(USECASE_FILENAME);
+//        Scanner scanner = new Scanner(inputStream);
+//
+//        int q = scanner.nextInt();
+//        int[][] queries = new int[q][2];
+//
+//        for (int i = 0; i < q; i++) {
+//            queries[i][0] = scanner.nextInt();
+//            queries[i][1] = scanner.nextInt();
+//        }
+//
+//        return queries;
+//    }
+//
+//    public static int [] loadResults() {
+//        InputStream inputStream = QueryManagerTest.class.getResourceAsStream(RESULTS_FILENAME);
+//        Scanner scanner = new Scanner(inputStream);
+//
+//        int q = scanner.nextInt();
+//        int[] results = new int[q];
+//
+//        for (int i = 0; i < q; i++) {
+//            results[i] = scanner.nextInt();
+//        }
+//
+//        return results;
+//    }
+//}
