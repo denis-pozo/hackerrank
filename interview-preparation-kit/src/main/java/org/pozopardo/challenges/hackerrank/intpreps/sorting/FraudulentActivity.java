f6dde003ce93ec3c10b3c6484e65dc017cdea252
Denis Pozo <denispzpd@gmail.de>
2019-08-01 17:30:00 +0200

    end fraudulent activity exercise

--- interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/sorting/FraudulentActivity.java
+++ interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/sorting/FraudulentActivity.java
@@ -10,38 +10,67 @@ import java.util.regex.*;
 
 public class FraudulentActivity {
 
+    private final static int SORT_RANGE = 201;
+
     // Complete the activityNotifications function below.
+    // The complexity of this algorithm is O(SORT_RANGE)
     static int activityNotifications(int[] expenditure, int d) {
-//        int [] subExpenditure = new int [d];
         Queue<Integer> subExpenditure = new LinkedList<>();
+        int [] occurrences = new int[SORT_RANGE];
 
         for(int i = 0 ; i < d ; i++) {
             subExpenditure.add(expenditure[i]);
+            occurrences[expenditure[i]]++;
         }
 
         int count = 0;
-        for(int i = d ; i < expenditure.length-1 ; i++) {
-            long median = calculateMedian(subExpenditure);
-            if(expenditure[i] >= median) count++;
-            subExpenditure.remove();
-            subExpenditure.add(expenditure[i++]);
+        for(int i = d ; i < expenditure.length ; i++) {
+            double median = calculateMedian(occurrences, d);
+            if(expenditure[i] >= 2 * median) count++;
+            int out = subExpenditure.remove();
+            occurrences[out]--;
+            subExpenditure.add(expenditure[i]);
+            occurrences[expenditure[i]]++;
         }
 
         return count;
     }
 
-    private static long calculateMedian(final Queue<Integer> expenditure) {
-        List<Integer> copy = new LinkedList<>(expenditure);
-        Collections.sort(copy);
+    private static double calculateMedian(int [] occurrences, int d) {
+        int [] occurSum = new int[SORT_RANGE];
+        occurSum[0] = occurrences[0];
 
-        if(copy.size() % 2 == 0) {
-            int half = copy.size() / 2;
-            int a = copy.get(half-1);
-            int b = copy.get(half);
+        for(int i = 1 ; i < occurrences.length ; i++){
+            occurSum[i] = occurSum[i-1] + occurrences[i];
+        }
+
+        double a = 0;
+        double b = 0;
+        if(d % 2 == 0) {
+            int first = d/2;
+            int second = first+1;
+            for(int i = 0 ; i < SORT_RANGE ; i++) {
+                if(first <= occurSum[i]) {
+                    a = i;
+                    break;
+                }
+            }
+            for(int i = 0 ; i < SORT_RANGE ; i++) {
+                if(second <= occurSum[i]) {
+                    b = i;
+                    break;
+                }
+            }
             return (a+b)/2;
         } else {
-            int half = Math.round(copy.size() / 2);
-            return copy.get(half);
+            int first = d/2 + 1;
+            for(int i = 0 ; i < SORT_RANGE ; i++) {
+                if(first <= occurSum[i]) {
+                    a = i;
+                    break;
+                }
+            }
+            return a;
         }
     }
 
