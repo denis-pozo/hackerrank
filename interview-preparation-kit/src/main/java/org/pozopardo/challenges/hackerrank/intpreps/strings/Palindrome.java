d6350c8e9ec8ab9ef4f934cd15e03c31d10e991f
Denis Pozo <denispzpd@gmail.de>
2019-08-18 15:57:37 +0200

    improve the version of the palindrome method

--- interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/strings/Palindrome.java
+++ interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/strings/Palindrome.java
@@ -10,48 +10,95 @@ import java.util.regex.*;
 
 public class Palindrome {
 
+    private static List<Integer> nSumcache = new ArrayList<>(Arrays.asList(0, 1, 3));
+
     // Complete the substrCount function below.
     static long substrCount(int n, String s) {
         char [] sArray = s.toCharArray();
         int count = 0;
 
-        for(int length = 1 ; length <= n ; length++) {
-            count += countPalindromes(sArray, length);
-        }
-
-        return count;
-    }
-
-    private static int countPalindromes(char[] sArray, int length) {
-        if (length == 1) return sArray.length;
-
-        int count = 0;
-        Deque<Character> list = new LinkedList<>();
+        char current = sArray[0];
+        int firstIndex = 0;
+        int middleIndex = 0;
         int i;
-        for(i = 0 ; i < length ; i++) {
-            list.add(sArray[i]);
+        for(i = 1 ; i < n ; i++) {
+            if(sArray[i] != current) {
+                if (middleIndex == 0) {
+                    middleIndex = i;
+                } else {
+                    count += computeSpecialSubstrings(firstIndex, middleIndex, i-1, n);
+                    firstIndex = middleIndex;
+                    current = sArray[firstIndex];
+                    middleIndex = 0;
+                    i = firstIndex;
+                }
+            }
         }
-        if(isSpecial(list)) count++;
-        if(sArray.length == length) return count;
-
-        do {
-            list.add(sArray[i++]);
-            list.removeFirst();
-            if(isSpecial(list)) count++;
-        } while (i < sArray.length);
+        count += computeSpecialSubstrings(firstIndex, middleIndex, i-1, n);
 
         return count;
     }
 
-    private static boolean isSpecial(Deque<Character> subString) {
-        Deque<Character> copy = new LinkedList<>(subString);
-        char first = copy.getFirst();
-        while(copy.size() > 1) {
-            if (copy.removeFirst() != first || copy.removeLast() != first) return false;
+    protected static int computeSpecialSubstrings(int firstIndex, int middleIndex, int lastIndex, int stringSize) {
+
+        if (middleIndex == 0) return summation(stringSize - firstIndex);
+
+        int frontEqualChars = middleIndex - firstIndex;
+        int rearEqualChars = lastIndex - middleIndex;
+
+        int count = summation (frontEqualChars);
+        count += Math.min(frontEqualChars, rearEqualChars);
+
+        if (lastIndex + 1 == stringSize) {
+            count += summation(rearEqualChars);
+            count++; // the middle char is itself a special substring
         }
 
-        return true;
+        return count;
     }
+
+    private static int summation(int n) {
+
+        if(nSumcache.size() > n) return nSumcache.get(n);
+        else {
+            for (int i = nSumcache.size() ; i <= n ; i++) {
+                nSumcache.add(i, i + nSumcache.get(i-1));
+            }
+        }
+
+        return nSumcache.get(n);
+    }
+//
+//    private static int countPalindromes(char[] sArray, int length) {
+//        if (length == 1) return sArray.length;
+//
+//        int count = 0;
+//        Deque<Character> list = new LinkedList<>();
+//        int i;
+//        for(i = 0 ; i < length ; i++) {
+//            list.add(sArray[i]);
+//        }
+//        if(isSpecial(list)) count++;
+//        if(sArray.length == length) return count;
+//
+//        do {
+//            list.add(sArray[i++]);
+//            list.removeFirst();
+//            if(isSpecial(list)) count++;
+//        } while (i < sArray.length);
+//
+//        return count;
+//    }
+//
+//    private static boolean isSpecial(Deque<Character> subString) {
+//        Deque<Character> copy = new LinkedList<>(subString);
+//        char first = copy.getFirst();
+//        while(copy.size() > 1) {
+//            if (copy.removeFirst() != first || copy.removeLast() != first) return false;
+//        }
+//
+//        return true;
+//    }
 
     private static final Scanner scanner = new Scanner(System.in);
 
