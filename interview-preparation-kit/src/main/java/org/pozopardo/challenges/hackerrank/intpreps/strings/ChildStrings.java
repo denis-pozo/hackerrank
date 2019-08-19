ed7f488b7e7a008583d5779a97002e90c03dccc4
Denis Pozo <denispzpd@gmail.de>
2019-08-20 00:51:09 +0200

    add exercise stub Common Child

---
+++ interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/strings/ChildStrings.java
@@ -0,0 +1,38 @@
+package org.pozopardo.challenges.hackerrank.intpreps.strings;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ChildStrings {
+
+    // Complete the commonChild function below.
+    static int commonChild(String s1, String s2) {
+
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s1 = scanner.nextLine();
+
+        String s2 = scanner.nextLine();
+
+        int result = commonChild(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
