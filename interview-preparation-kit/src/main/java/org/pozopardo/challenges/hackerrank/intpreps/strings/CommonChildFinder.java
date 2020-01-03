6acaa40be560eb3cdbd9905cf641d12aa92669a8
Denis Pozo <denispzpd@gmail.com>
2020-01-03 16:28:11 +0100

    Finish first approach, still room for improvement (next: reverse search)

--- interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/strings/CommonChildFinder.java
+++ interview-preparation-kit/src/main/java/org/pozopardo/challenges/hackerrank/intpreps/strings/CommonChildFinder.java
@@ -1,7 +1,6 @@
 package org.pozopardo.challenges.hackerrank.intpreps.strings;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
@@ -10,8 +9,54 @@ public class CommonChildFinder {
     // Complete the commonChild function below.
     static int commonChild(String s1, String s2) {
 
-        return -1;
+        if(s1.equals(s2)) return s1.length();
 
+        int size1 = commonChildSize(s1, s2);
+        int size2 = commonChildSize(s2, s1);
+
+        if(size1 > size2) {
+            return size1;
+        } else {
+            return size2;
+        }
+        // XXX This solution does not apply when the first common character
+        // does not return the biggest common child (see last test case).
+        // TODO Reverse search for the next common.
+    }
+
+    private static int commonChildSize(String s1, String s2) {
+        int count = 0;
+        int startPos = 0;
+        int nextPos = 0;
+
+        for(char c : s1.toCharArray()) {
+            nextPos = findNext(c, s2.substring(startPos));
+
+            if (nextPos == -1) {
+                continue;
+            }
+
+            startPos += nextPos + 1;
+            count++;
+
+            if (startPos == s2.length()) {
+                return count;
+            }
+        }
+
+        return count;
+    }
+
+    private static int findNext(char toFind, String substring) {
+        char [] subArray = substring.toCharArray();
+
+        for(int i = 0 ; i < substring.length() ; i++) {
+            if(toFind == subArray[i]) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
--- interview-preparation-kit/src/test/java/org/pozopardo/challenges/hackerrank/intpreps/strings/CommonChildFinderTest.java
+++ interview-preparation-kit/src/test/java/org/pozopardo/challenges/hackerrank/intpreps/strings/CommonChildFinderTest.java
@@ -1,4 +1,58 @@
 package org.pozopardo.challenges.hackerrank.intpreps.strings;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class CommonChildFinderTest {
+
+    @Test
+    public void should_equalStrings_returnStringLength () {
+        String s = "ABC";
+        int result = CommonChildFinder.commonChild(s, s);
+
+        assertEquals(s.length(), result);
+    }
+
+    @Test
+    public void should_stringOneCharEqual_thenReturnOne() {
+        String s1 = "AB";
+        String s2 = "BC";
+        int result = CommonChildFinder.commonChild(s1, s2);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void should_harryAndSallyComparison_thenReturnTwo() {
+        String s1 = "HARRY";
+        String s2 = "SALLY";
+
+        assertEquals(2, CommonChildFinder.commonChild(s1, s2));
+    }
+
+    @Test
+    public void should_differentSringsBeCompared_thenReturn0() {
+        String s1 = "AA";
+        String s2 = "BB";
+
+        assertEquals(0, CommonChildFinder.commonChild(s1, s2));
+    }
+
+    @Test
+    public void should_shinchanAndNoharaaaBeCompared_thenReturn3() {
+        String s1 = "SHINCHAN";
+        String s2 = "NOHARAAA";
+
+        assertEquals(3, CommonChildFinder.commonChild(s1, s2));
+    }
+
+    @Test
+    public void should_ABCDEFandFBDAMN_thenReturn2() {
+        String s1 = "ABCDEF" ;
+        String s2 = "FBDAMN";
+
+        assertEquals(2, CommonChildFinder.commonChild(s2, s1));
+    }
 }
